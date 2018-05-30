ee2b60bc002dc42979d16c7b30f6d0bdf723ed35
Trive-VAIO\Trive <triventrive@gmail.com>
2018-05-30 21:25:50 +0200

    Initial commit

---
+++ src/tests/action/expenses.test.js
@@ -0,0 +1,51 @@
+import { addExpense, removeExpense, editExpense } from '../../actions/expenses';
+
+test('should setup remove expense action object', () => {
+    const action = removeExpense({ id: '123abc' });
+    expect(action).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id: '123abc'
+    });
+});
+
+test('should setup edit expense action object', () => {
+    const action = editExpense('abc123', { note: 'A new note' });
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: 'abc123',
+        updates: { 
+            note: 'A new note' 
+        }
+    });
+});
+
+test('should setup add expense action object with provided values', () => {
+    const expenseData = {
+        description: 'Rent',
+        amount: 109500,
+        createdAt: 1000,
+        note: 'This was last months last rent'
+    };
+    const action = addExpense(expenseData);
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            id: expect.any(String)
+        }
+    });
+});
+
+test('should setup add expense action object with defaults values', () => {
+    const action = addExpense();
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            id: expect.any(String),
+            description: '', 
+            note: '', 
+            amount: 0, 
+            createdAt: 0 
+        }
+    });
+});
