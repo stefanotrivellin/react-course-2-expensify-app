4910bae0ea1c9fd2852a3c8e28eba79d1aebde98
Trive-VAIO\Trive <triventrive@gmail.com>
2018-06-03 14:42:22 +0200

    Add expense summary to dashboard

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,18 +4,22 @@ import selectExpenses from '../selectors/expenses';
 import getExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 
-const ExpensesSummary = (props) => (
-    <div>
-        Viewing {props.expensesCount} {props.expensesCount === 1 
-            ? 'expense' : 'expenses'} totalling {props.expensesTotal}
-    </div>
-);
+export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
+    const expenseWord = expensesCount === 1 ? 'expense' : 'expenses';
+    const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+    return(
+        <div>
+            <h1>Viewing {expensesCount} {expenseWord} totalling {formattedExpensesTotal}
+            </h1>
+        </div>
+    );
+};
 
 const mapStateToProps = (state) => {
-    const expenses = selectExpenses(state.expenses, state.filters);
+    const visibleExpenses = selectExpenses(state.expenses, state.filters);
     return {
-        expensesCount: expenses.length,
-        expensesTotal: numeral(getExpensesTotal(expenses) / 100).format('$0,0.00')
+        expensesCount: visibleExpenses.length,
+        expensesTotal: getExpensesTotal(visibleExpenses)
     };
 };
 
