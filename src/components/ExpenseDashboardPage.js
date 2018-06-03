700685a6ed46061fe8cbf048a8e594f23a0e8ce2
Trive-VAIO\Trive <triventrive@gmail.com>
2018-06-03 13:40:30 +0200

    Add ExpensesSummary component with tests

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
-    <div>        
-        <ExpenseListFilters />
+    <div>
+        <ExpensesSummary />
+        <ExpenseListFilters />        
         <ExpenseList />
     </div>
 );
