919ba619e2dfb523c58ef89677592a3a5a599662
Trive-VAIO\Trive <triventrive@gmail.com>
2018-06-17 14:25:34 +0200

    Add addExpenses and startAddExpenses actions

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -38,3 +38,26 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const expenses = [];
+    
+            snapshot.forEach((expense) => {
+                expenses.push({
+                    id: expense.key,
+                    ...expense.val()
+                });
+            });
+    
+            dispatch(setExpenses(expenses));
+        });
+    };
+};
